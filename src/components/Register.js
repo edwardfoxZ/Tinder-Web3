058f0f5345fcfea6e9e8e9c8b30887326d6ca5a5
CodeEdward <108403615+edwardfox15@users.noreply.github.com>
2025-01-22 21:30:18 +0000

    4-front

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { CityEntries } from "../utils/CityEntries";
 import Select from "react-select";
+import { AiOutlineUpload } from "react-icons/ai";
+import { CiCircleRemove } from "react-icons/ci";
 
 export const Register = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleUploadImage = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImageUrl(url);
+    }
+  };
+
+  const removeTheImage = () => {
+    setImageUrl(null);
+  };
+
   const options = [
     { value: 0, label: "Female" },
     { value: 1, label: "Male" },
@@ -29,7 +45,33 @@ export const Register = () => {
     <div className="Register flex">
       <div className="Form flex flex-column items-center">
         <div className="Title flex flex-row justify-space-between">
-          <span className="Profile flex"></span>
+          <span className="Profile flex">
+            {imageUrl ? (
+              <img draggable={false} src={imageUrl} alt="userprofile" />
+            ) : (
+              <>
+                <input
+                  id="upload-file"
+                  accept="image/*"
+                  onChange={handleUploadImage}
+                  type="file"
+                />
+                <label htmlFor="upload-file" className="cursor-pointer">
+                  <AiOutlineUpload color="white" fontSize="30px" />
+                </label>
+              </>
+            )}
+          </span>
+          <span>
+            {imageUrl && (
+              <CiCircleRemove
+                onClick={removeTheImage}
+                className="cursor-pointer"
+                color="white"
+                size="25"
+              />
+            )}
+          </span>
           <h1>Welcome to Tinder</h1>
         </div>
         <div className="Form-detail flex-row">
@@ -45,8 +87,10 @@ export const Register = () => {
           />
         </div>
         <CityEntries />
-        <div className="Sign-btn">
-          <button>Sign in</button>
+        <div className="Sign-in-container flex flex-column">
+          <button className="Sing-up-btn">Sign up</button>
+          <a href="">you haven't account let's make one</a>
+          <p>® The privacy has been declared for.</p>
         </div>
       </div>
     </div>
